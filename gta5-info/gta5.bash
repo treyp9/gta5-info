c8cf1291883f725f07f3a87e48b07b2f902b8610
treyp9 <idk14305@gmail.com>
2026-09-11 01:34:34 -0600

    Add files via upload

---
+++ gta5-info/gta5.bash
@@ -0,0 +1,26 @@
+#!/bin/bash
+figlet "Description" | lolcat
+sleep 2
+boxes gta-description | lolcat
+sleep 15
+clear
+figlet "Cheats" | lolcat
+sleep 2
+boxes gta-cheats | lolcat
+sleep 15
+clear 
+figlet "Weapons" | lolcat
+sleep 2
+boxes gta-weapons | lolcat
+sleep 15
+clear
+figlet "Places" | lolcat
+sleep 2
+boxes gta-places | lolcat
+sleep 15
+clear
+figlet "Missions" | lolcat
+sleep 2
+boxes gta-missions | lolcat
+sleep 15
+clear
